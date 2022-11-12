5696de9ad5761a26259d92e285081099e22a5470
habib24457 <habiburehman390@gmail.com>
2022-11-12 11:12:53 +0100

    Updated API

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,80 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import './Home.css';
-import { Button, Navbar, Nav, Container, Card } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import { Button, Navbar, Nav, Container, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import ReactLoading from 'react-loading';
-import Footer from '../Footer/Footer';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-
-
+import ReactLoading from "react-loading";
+import Footer from "../Footer/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { API } from "../Constant/Constant";
 
 const Home = () => {
-    const [products, setProducts] = useState([]);
-    const [done, setDone] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [done, setDone] = useState(false);
 
+  useEffect(() => {
+    fetch(API + "/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+        setDone(true);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    useEffect(() => {
-        fetch('https://lychee-surprise-09798.herokuapp.com/products')
-            .then(res => res.json())
-            .then(data => {
-                setProducts(data)
-                setDone(true);
-            })
+  return (
+    <Container>
+      <div className="row">
+        <div className="col-md-12">
+          <Navbar className="fixed-top">
+            <Navbar.Brand className="text-success" href="/home">
+              Picnic Super Market
+            </Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end">
+              <Nav>
+                <Nav.Link href="/home">Home</Nav.Link>
+                <Nav.Link href="/order">Orders</Nav.Link>
+                <Nav.Link href="/continue">Admin</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      </div>
+      {!done ? (
+        <div className="row justify-content-center mt-5 pt-5">
+          <ReactLoading
+            type={"spin"}
+            color={"green"}
+            height={200}
+            width={200}
+          />
+        </div>
+      ) : (
+        <div className="row justify-content-center mt-3 pt-5 mb-5">
+          {products.map((product) => (
+            <div>
+              <Card
+                className="p-3 m-3"
+                style={{ width: "18rem", height: "30rem" }}
+              >
+                <Card.Img variant="top" src={product.imageURL} />
+                <Card.Body>
+                  <Card.Title>{product.name}</Card.Title>
+                  <Card.Text>Price:{product.price}$</Card.Text>
+                  <Card.Text>Weight:{product.weight}</Card.Text>
 
-    }, [])
-
-
-  
-    return (
-        <Container>
-            <div className="row">
-                <div className="col-md-12">
-                    <Navbar className="fixed-top">
-                        <Navbar.Brand className="text-success" href="/home">Picnic Super Market</Navbar.Brand>
-                        <Navbar.Collapse className="justify-content-end">
-                            <Nav>
-                                <Nav.Link href="/home">Home</Nav.Link>
-                                <Nav.Link href="/order">Orders</Nav.Link>
-                                <Nav.Link href="/continue">Admin</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Navbar>
-                </div>
+                  <Link to={`/selectedProduct/${product._id}`}>
+                    <Button className="buy-button">
+                      <FontAwesomeIcon icon={faShoppingCart} size="1x" />
+                      Add to cart
+                    </Button>
+                  </Link>
+                </Card.Body>
+              </Card>
             </div>
-            {
-                !done ?
-                    <div className="row justify-content-center mt-5 pt-5">
-                        <ReactLoading type={"spin"} color={"green"} height={200} width={200} />
-                    </div>
-                    :
-                    <div className="row justify-content-center mt-3 pt-5 mb-5">
-                        {
-                            products.map(product => <div>
-                                <Card className="p-3 m-3" style={{ width: '18rem', height: '30rem' }}>
-                                    <Card.Img variant="top" src={product.imageURL} />
-                                    <Card.Body>
-                                        <Card.Title>{product.name}</Card.Title>
-                                        <Card.Text>Price:{product.price}$</Card.Text>
-                                        <Card.Text>Weight:{product.weight}</Card.Text>
+          ))}
+        </div>
+      )}
 
-                                        <Link to={`/selectedProduct/${product._id}`}>
-                                            <Button className="buy-button">
-                                            <FontAwesomeIcon icon={faShoppingCart} size="1x" />
-                                                Add to cart
-                                                </Button>
-                                        </Link>
-
-                                    </Card.Body>
-                                </Card>
-                            </div>)
-                        }
-                    </div>
-            }
-
-            <Footer></Footer>
-        </Container>
-    );
+      <Footer></Footer>
+    </Container>
+  );
 };
 
 export default Home;
--- src/Components/ManageProducts/ManageProducts.js
+++ src/Components/ManageProducts/ManageProducts.js
@@ -1,97 +1,125 @@
-import React, { useState } from 'react';
-import './ManageProducts.css';
+import React, { useState } from "react";
+import "./ManageProducts.css";
 import { useForm } from "react-hook-form";
 import { Nav, Navbar, Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Footer from '../Footer/Footer';
+import Footer from "../Footer/Footer";
+import { API } from "../Constant/Constant";
 
-const axios = require('axios').default;
+const axios = require("axios").default;
 const ManageProducts = () => {
-    const { register, handleSubmit, errors } = useForm();
-    const [imageURL, setImageURL] = useState(null);
+  const { register, handleSubmit, errors } = useForm();
+  const [imageURL, setImageURL] = useState(null);
 
-    const onSubmit = data => {
-        const productData = {
-            name: data.productName,
-            price: data.productPrice,
-            weight: data.productWeight,
-            imageURL: imageURL
-        }
-        const url = `https://lychee-surprise-09798.herokuapp.com/addProduct`;
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(productData)
-        })
-            .then(response => {
-                alert("Product saved", response);
-
-            });
+  const onSubmit = (data) => {
+    const productData = {
+      name: data.productName,
+      price: data.productPrice,
+      weight: data.productWeight,
+      imageURL: imageURL,
     };
+    const url = API + "/addProduct";
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    })
+      .then((response) => {
+        alert("Product saved", response);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const handleImageUpload = (event) => {
-        console.log(event.target.files[0]);
-        const imageData = new FormData();
-        imageData.set('key', 'f7675ca9a0ed0f2cd7c6d55c64d847b9');
-        imageData.append('image', event.target.files[0])
+  const handleImageUpload = (event) => {
+    console.log(event.target.files[0]);
+    const imageData = new FormData();
+    imageData.set("key", "f7675ca9a0ed0f2cd7c6d55c64d847b9");
+    imageData.append("image", event.target.files[0]);
 
-        axios.post('https://api.imgbb.com/1/upload', imageData)
-            .then(function (response) {
-                setImageURL(response.data.data.display_url);
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-    }
+    axios
+      .post("https://api.imgbb.com/1/upload", imageData)
+      .then(function (response) {
+        setImageURL(response.data.data.display_url);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-    const handleLogout = () => {
-        console.log('Clicked');
-        sessionStorage.removeItem('token');
-        sessionStorage.removeItem('email');
-    }
+  const handleLogout = () => {
+    console.log("Clicked");
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("email");
+  };
 
-    return (
-        <Container>
-            <div className="row">
-                <div className="col-md-12">
-                    <Navbar bg="light" variant="light">
-                        <Navbar.Brand className="text-success" href="/home">Picnic Super Market</Navbar.Brand>
-                        <Navbar.Collapse className="justify-content-end">
-                            <Nav>
-                                <Nav.Link href="/addAdmin">Add-New-Admin</Nav.Link>
-                                <Nav.Link href="/update">Update-Products</Nav.Link>
-                                <Nav.Link href="/manage">Add-Product</Nav.Link>
-                                <Link to="/home">
-                                    <Button onClick={handleLogout} className="btn btn-success">Log Out</Button>
-                                </Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Navbar>
-                </div>
-            </div>
+  return (
+    <Container>
+      <div className="row">
+        <div className="col-md-12">
+          <Navbar bg="light" variant="light">
+            <Navbar.Brand className="text-success" href="/home">
+              Picnic Super Market
+            </Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end">
+              <Nav>
+                <Nav.Link href="/addAdmin">Add-New-Admin</Nav.Link>
+                <Nav.Link href="/update">Update-Products</Nav.Link>
+                <Nav.Link href="/manage">Add-Product</Nav.Link>
+                <Link to="/home">
+                  <Button onClick={handleLogout} className="btn btn-success">
+                    Log Out
+                  </Button>
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      </div>
 
-            <div className="row justify-content-center mt-5 pt-5">
-                <form className="form-group" onSubmit={handleSubmit(onSubmit)} >
-                    <h4 className="mb-5">Add new products to the database.</h4>
-                    <input className="form-control" name="productName" placeholder="Product Name" ref={register} required />
-                    <br />
-                    <input className="form-control" name="productPrice" placeholder="Price" ref={register} required />
-                    <br />
-                    <input className="form-control" name="productWeight" placeholder="Weight" ref={register} required />
-                    <br />
-                    <input name="exampleRequired" type="file" onChange={handleImageUpload} />
-                    <br />
-                    {errors.exampleRequired && <span>This field is required</span>}
-                    <br />
-                    <input className="button-design" type="submit" />
-                </form>
-            </div>
+      <div className="row justify-content-center mt-5 pt-5">
+        <form className="form-group" onSubmit={handleSubmit(onSubmit)}>
+          <h4 className="mb-5">Add new products to the database.</h4>
+          <input
+            className="form-control"
+            name="productName"
+            placeholder="Product Name"
+            ref={register}
+            required
+          />
+          <br />
+          <input
+            className="form-control"
+            name="productPrice"
+            placeholder="Price"
+            ref={register}
+            required
+          />
+          <br />
+          <input
+            className="form-control"
+            name="productWeight"
+            placeholder="Weight"
+            ref={register}
+            required
+          />
+          <br />
+          <input
+            name="exampleRequired"
+            type="file"
+            onChange={handleImageUpload}
+          />
+          <br />
+          {errors.exampleRequired && <span>This field is required</span>}
+          <br />
+          <input className="button-design" type="submit" />
+        </form>
+      </div>
 
-            <Footer></Footer>
-        </Container>
-    );
+      <Footer></Footer>
+    </Container>
+  );
 };
 
 export default ManageProducts;
